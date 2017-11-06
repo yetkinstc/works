c9446ca8ec2ae2468a769eb0176ed5b38ee69c1c
yetkinstc <32813615+yetkinstc@users.noreply.github.com>
2017-11-06 06:48:38 +0300

    Add files via upload

---
+++ harder/SpeakGoodBye.js
@@ -0,0 +1,40 @@
+// NOTE! The steps in this file are basically identical to the ones you
+// performed in the SpeakHello.js file.
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+// STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
+// See Lecture 52, part 2
+
+(function (name) {
+	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  console.log("Hello " + name);
+});
+// STEP 7: Create an object, called 'byeSpeaker' to which you will attach
+// the "speak" method and which you will expose to the global context
+// See Lecture 52, part 1
+var byeSpeaker = {};
+
+
+// DO NOT attach the speakWord variable to the 'byeSpeaker' object.
+var speakWord = "Good Bye";
+
+// STEP 8: Rewrite the 'speak' function such that it is attached to the
+// byeSpeaker object instead of being a standalone function.
+// See Lecture 52, part 2
+function speak(name) {
+
+   console.log(speakWord + " " + name);
+  var byeSpeaker = {};
+ byeSpeaker.name=name;
+  byeSpeaker.speakWord = function () {
+    console.log("good bye" + byeSpeaker.name);
+  }
+  
+
+  window.byeSpeaker = byeSpeaker;
+}
+
+
+
+// STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
+// 'byeSpeaker' on the global scope as well.
+// xxxx.xxxx = byeSpeaker;
